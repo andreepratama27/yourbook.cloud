ec9e6add79f904b304b2032c5a4db45c299d77d3
andre7131041 <andre@primaku.com>
2024-06-06 10:16:00 +0700

    feat: implement homepage

---
+++ src/services/book.service.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-useless-catch */
+import axios from "../libs/axios";
+
+export interface Book {
+	id: number;
+	title: string;
+	description: string;
+	cover: string;
+	publicationDate: string;
+	author: string;
+}
+
+interface Params {
+	offset: number;
+	limit: number;
+}
+
+const getBook = async ({ offset = 0, limit = 5 }: Params): Promise<Book[]> => {
+	try {
+		const response = await axios.get(`/books?_start=${offset}&_limit=${limit}`);
+		return response.data;
+	} catch (error) {
+		// biome-ignore lint/complexity/noUselessCatch: <explanation>
+		throw error;
+	}
+};
+
+const getBookDetail = async (id: string) => {
+	try {
+		const response = await axios.get(`/books/${id}`);
+		return response.data;
+	} catch (error) {
+		// biome-ignore lint/complexity/noUselessCatch: <explanation>
+		throw error;
+	}
+};
+
+export { getBook, getBookDetail };
